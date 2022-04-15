78b657d55053c3315fd857dabf6c5fa8575a0072
skrawczyk <skrawczyk@stitchfix.com>
2022-04-15 10:39:45 -0700

    Updates condensed numeric transforms
    To match 1.6.0 hamilton release.

--- absenteeism/normalized_features_condensed.py
+++ absenteeism/normalized_features_condensed.py
@@ -1,36 +1,31 @@
-from hamilton.function_modifiers import parametrized_input
+from hamilton.function_modifiers import parametrized_inputs
 import pandas as pd
 
 NUMERIC_FEATURES = ['age', 'height', 'weight', 'body_mass_index', 'transportation_expense',
                     'distance_from_residence_to_work', 'service_time', 'work_load_average_per_day',
                     'hit_target']
-FEATURE_MEANS = {k: (f'{k}_mean', f'Average of {k}') for k in NUMERIC_FEATURES}
-FEATURE_STD_DEV = {k: (f'{k}_std_dev', f'Standard deviation of {k}') for k in NUMERIC_FEATURES}
 
 
-@parametrized_input(parameter='feature', variable_inputs=FEATURE_MEANS)
+@parametrized_inputs(**{f'{k}_mean': dict(feature=k) for k in NUMERIC_FEATURES})
 def mean_computer(feature: pd.Series) -> pd.Series:
+    """Average of {feature}"""
     return feature.mean()
 
 
-@parametrized_input(parameter='feature', variable_inputs=FEATURE_STD_DEV)
+@parametrized_inputs(**{f'{k}_std_dev': dict(feature=k) for k in NUMERIC_FEATURES})
 def std_dev_computer(feature: pd.Series) -> pd.Series:
+    """Standard deviation of {feature}"""
     return feature.std()
 
 
-# TODO: enable hamilton to do this multiple column parameterization?
-FEATURE_ZERO_MEAN = {(k, f'{k}_mean'): (f'{k}_zero_mean', f'Zero mean of {k}') for k in NUMERIC_FEATURES}
-FEATURE_ZERO_MEAN_UNIT_VAR = {
-    (f'{k}_zero_mean', f'{k}_std_dev'): (f'{k}_zero_mean_unit_var', f'Zero mean unit variance value of {k}')
-    for k in NUMERIC_FEATURES
-}
-
-
-@parametrized_input('feature', 'feature_mean', variable_inputs=FEATURE_ZERO_MEAN)
+@parametrized_inputs(**{f'{k}_zero_mean': dict(feature=k, feature_mean=f'{k}_mean') for k in NUMERIC_FEATURES})
 def zero_mean_computer(feature: pd.Series, feature_mean: pd.Series) -> pd.Series:
+    """Creates zero mean value of {feature} by subtracting {feature_mean}"""
     return feature - feature_mean
 
 
-@parametrized_input('feature_mean', 'feature_std_dev', variable_inputs=FEATURE_ZERO_MEAN_UNIT_VAR)
+@parametrized_inputs(**{f'{k}_zero_mean_unit_var': dict(feature_mean=f'{k}_mean', feature_std_dev=f'{k}_std_dev')
+                        for k in NUMERIC_FEATURES})
 def zero_mean_unit_var_computer(feature_mean: pd.Series, feature_std_dev: pd.Series) -> pd.Series:
+    """Computes zero mean unit variance of {feature_mean} with {feature_std_dev} to create {output_name}"""
     return feature_mean / feature_std_dev
